debf5477f4ba8dd524e3e5fa16dc14bb20c0984e
熊哥 <dppss0118@gmail.com>
2025-07-09 17:13:47 +0800

    refactor

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import discord
 from discord.ext import commands
 import json
-
+import random
 with open('setting.json', 'r', encoding='utf-8') as jfile:
-    jdata = json.load(jfile)
-    
+    jdata1 = json.load(jfile)
+with open('other_use.json', 'r', encoding='utf-8') as jfile:
+    jdata2 = json.load(jfile)    
 DISCORD_BOT_TOKEN = None #備用
 intents = discord.Intents.default()
 intents.members = True #啟用成員相關的事件
@@ -17,19 +18,27 @@ async def on_ready():#async def 義務函數
     
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(int(jdata["Welcome_Channel_ID"]))#從json檔中讀取歡迎頻道ID(json檔回傳的是字串，所以要轉成int)
+    channel = bot.get_channel(int(jdata1["Welcome_Channel_ID"]))#從json檔中讀取歡迎頻道ID(json檔回傳的是字串，所以要轉成int)
     await channel.send(f"歡迎 {member.mention} 來到伺服器！")#用await是因為協成
+    
 @bot.command() #指令
 async def ping(ctx): #ctx是上下文 ex: A:嗨 (使用者,id,所在伺服器,所在頻道) B:早安
     await ctx.send(f"延遲時間:{round(bot.latency*1000,2)}(ms)") #bot.latency是bot的延遲時間，ctx.send是發送訊息到提出指令頻道
+
 @bot.command() #指令
 async def 圖片(ctx):#傳送圖片的指令區
-    pic = discord.File("D:\\dc_bot\\picture\\picture.png") #圖片路徑
+    random_pic = random.choice(jdata2["pic1"])   #從json檔中隨機選擇一張圖片
+    pic = discord.File(random_pic) #discord.File是discord.py中用來處理檔案的類別，括號中填入圖片的路徑 
     await ctx.send(file = pic)  #ctx.send是發送訊息到提出指令頻道，file是發送檔案 
+
+async def web_pic(ctx):
+    random_pic = random.choice(jdata2["web_pic1"])   #從json檔中隨機選擇一張圖片
+    pic = discord.File(random_pic) #discord.File是discord.py中用來處理檔案的類別，括號中填入圖片的路徑 
+    await ctx.send(file = pic)  #ctx.send是發送訊息到提出指令頻道，file是發送檔案
 
 @bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(int(jdata["Leave_Channel_ID"]))
+    channel = bot.get_channel(int(jdata1["Leave_Channel_ID"]))
     await channel.send(f"{member.mention} 離開伺服器！")#用await是因為協成
-bot.run(jdata['TOKEN'])#bot啟動，並在括弧中填入token
+bot.run(jdata1['TOKEN'])#bot啟動，並在括弧中填入token
 #如果他不是loop執行完會閃退
