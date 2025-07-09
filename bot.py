0054b8cf28f1db2d271d0ba853507415c7372992
熊哥 <dppss0118@gmail.com>
2025-07-09 16:39:00 +0800

    feat

--- bot.py
+++ bot.py
@@ -19,7 +19,9 @@ async def on_ready():#async def 義務函數
 async def on_member_join(member):
     channel = bot.get_channel(int(jdata["Welcome_Channel_ID"]))#從json檔中讀取歡迎頻道ID(json檔回傳的是字串，所以要轉成int)
     await channel.send(f"歡迎 {member.mention} 來到伺服器！")#用await是因為協成
-    
+@bot.command() #指令
+async def ping(ctx): #ctx是上下文 ex: A:嗨 (使用者,id,所在伺服器,所在頻道) B:早安
+    await ctx.send(f"延遲時間:{round(bot.latency*1000,2)}(ms)") #bot.latency是bot的延遲時間，ctx.send是發送訊息到頻道
 @bot.event
 async def on_member_remove(member):
     channel = bot.get_channel(int(jdata["Leave_Channel_ID"]))
